ec79a3c484515438e6318fd933841e388f948e32
Hikarukurosawa123 <h.kurosawa@mail.utoronto.ca>
2024-06-03 16:14:56 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -37,8 +37,8 @@ if uploaded_file is not None:
 				os.chdir(temp_dir)
 
 				decompressed_path = temp_lzo_path.replace('.lzo', '')
-				command_str = 'lzop -d '+ str(temp_lzo_path) + ' -o ' + str(decompressed_path)
-				
+				command_str = 'lzop -d '+ str(extract.name) + ' -o ' + str(decompressed_path)
+				os.system(command_str)
 				os.system('ls')
 
 
