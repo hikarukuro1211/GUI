3fc05f881e720fb16e2de1797f2b33165e535537
Hikarukurosawa123 <h.kurosawa@mail.utoronto.ca>
2024-06-03 21:56:02 -0400

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -7,7 +7,7 @@ from os import listdir
 import tempfile 
 import subprocess 
 import urllib
-#from oct2py import Oct2Py
+from oct2py import Oct2Py
 
 import matplotlib.pyplot as plt
 from io import BytesIO
@@ -18,8 +18,11 @@ from bs4 import BeautifulSoup
 import base64
 import json 
 
+#defining .txt file locations 
 rf_image_url = 'https://raw.githubusercontent.com/hikarukuro1211/GUI/main/rf_image.txt'
-
+rdataread_url = 'https://raw.githubusercontent.com/hikarukuro1211/GUI/main/rdataread.txt'
+ReadClariusYML_url = 'https://raw.githubusercontent.com/hikarukuro1211/GUI/main/ReadClariusYML.txt'
+hilbert_url = 'https://raw.githubusercontent.com/hikarukuro1211/GUI/main/hilbert.txt'
 
 st.title('Data Visualizer')
 st.sidebar.subheader('Upload a file')
@@ -55,6 +58,7 @@ if uploaded_file is not None:
 				os.system(command_str)							
 				os.system('ls')
 
+				#clear up the tempfolder once done 
 				os.remove(tail)
 				os.remove(decompressed_path)
 
@@ -62,20 +66,36 @@ if uploaded_file is not None:
 
 				with open("rf_image.m","w+") as f:
 					f.write(rf_image_content_web)
-					
-				os.system('pwd')
 
-				os.system('ls')
+				rdataread_content_web = requests.get(rdataread_url, stream = True).text
+
+				with open("rdataread.m","w+") as f:
+					f.write(rdataread_content_web)
+
+				ReadClariusYML_content_web = requests.get(ReadClariusYML_url, stream = True).text
+
+				with open("ReadClariusYML.m","w+") as f:
+					f.write(ReadClariusYML_content_web)
+
+				hilbert_content_web = requests.get(hilbert_url, stream = True).text
+
+				with open("hilbert.m","w+") as f:
+					f.write(hilbert_content_web)
+					
+				#os.system('pwd')
+
+				#os.system('ls')
+
+
+				oc = Oct2Py()
+				cur_dir = temp_dir
+				file_mame = entry.name.replace('.lzo', '')
+				x,y,z = oc.rf_image('', file_mame, nout = 3)
+				st.write(x,y,z)
+
+			#plt.imshow(z, extent=[15, 70, 15, 70], cmap = 'gray')
+			#plt.show()
+
 
 
 		os.chdir(temp_dir)
-
-
-	#create .m files for each matlab function text 
-	#rf_image_content_web = json.loads(requests.get(rf_image_url, stream = True).text)
-	#rf_image_content = ''
-	#for line in rf_image_content_web:
-	#	rf_image_content += line
-
-
-	#os.system('ls')
